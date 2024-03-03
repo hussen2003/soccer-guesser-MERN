e8534976f2c8e67fccd073cf50484dafa1e38901
Patrick Rizkalla <patrickrizkalla@yahoo.com>
2024-03-03 17:27:30 -0500

    added updateScore endpoint

--- controllers/landingController.js
+++ controllers/landingController.js
@@ -7,25 +7,7 @@ const leaderboardAmount = 10;
 export const leaderboard = async (req, res) => {
     try {
       //const { score } = req.body;
-        const users = await User.find({}).select({ "username": 1, "score": 1, "_id": 0}).sort({ score: 1 }).limit(20);
-  
-      
-
-    //   if (!user) {
-    //     return res.status(400).json({ error: "Username does not exist" });
-    //   }
-  
-    //   if (password != user.password) {
-    //     return res.status(400).json({ error: "Incorrect password" });
-    //   }
-  
-    //   res.status(201).json({
-    //     _id: user._id,
-    //     name: user.name,
-    //     email: user.email,
-    //     username: user.username,
-    //     score: user.score,
-    //   });
+        const users = await User.find({}).select({ "username": 1, "score": 1, "_id": 0}).sort({ score: -1 }).limit(20);
 
         res.status(201).json(users);
 
@@ -33,4 +15,20 @@ export const leaderboard = async (req, res) => {
       console.log("Error in login controller", error.message);
       res.status(500).json({ error: "Internal Server Error" });
     }
-  };
+};
+
+export const updateScore = async (req, res) => {
+    try {
+        const { username, score } = req.body;
+
+        await User.updateOne({ username }, { score: score })
+
+        res.status(201).json({
+            score: score || 0,
+          });
+
+    } catch (error) {
+      console.log("Error in login controller", error.message);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+};
