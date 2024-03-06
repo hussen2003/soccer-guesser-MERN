db128cfcac696fc47a2f99a92485cd9fd2ccf25b
raulgrat <ra379063@ucf.edu>
2024-03-06 00:39:13 -0500

    Add pictures and formating

--- frontend/src/pages/AboutPage.js
+++ frontend/src/pages/AboutPage.js
@@ -4,14 +4,14 @@ import './aboutpage.css'; // Importing CSS for styling
 
 function AboutPage() {
     const teamMembers = [
-        { name: 'David Urrego', role: 'Frontend (Web)' },
-        { name: 'Hussen Premier', role: 'Database' },
-        { name: 'Jack Gao', role: 'Frontend' },
-        { name: 'Luckner Ablard', role: 'Frontend (Mobile)' },
-        { name: 'Moses Daniel Cohen', role: 'Frontend (Web)' },
-        { name: 'Patrick Rizkalla', role: 'API' },
-        { name: 'Raul Graterol', role: 'Frontend (Mobile)' },
-        { name: 'Ryan Rahrooh', role: 'Project Manager' }
+        { name: 'David Urrego', role: 'Frontend (Web)', image: 'https://cdn.sofifa.net/players/241/721/24_120.png' },
+        { name: 'Hussen Premier', role: 'Database', image: 'https://cdn.sofifa.net/players/192/448/24_120.png' },
+        { name: 'Jack Gao', role: 'Frontend (Web)/(Mobile)', image: 'https://cdn.sofifa.net/players/232/411/24_120.png' },
+        { name: 'Luckner Ablard', role: 'Frontend (Mobile)', image: 'https://cdn.sofifa.net/players/206/517/24_120.png' },
+        { name: 'Daniel Cohen', role: 'Frontend (Web)', image: 'https://cdn.sofifa.net/players/231/443/24_120.png' },
+        { name: 'Patrick Rizkalla', role: 'API', image: 'https://cdn.sofifa.net/players/264/240/24_120.png' },
+        { name: 'Raul Graterol', role: 'Frontend (Mobile)', image: 'https://cdn.sofifa.net/players/252/371/24_120.png' },
+        { name: 'Ryan Rahrooh', role: 'Project Manager', image: 'https://cdn.sofifa.net/players/216/352/24_120.png' }
     ];
 
     function handleMouseEnter(event) {
@@ -35,6 +35,7 @@ function AboutPage() {
                         <div key={index} className="member">
                             <h2>{member.name}</h2>
                             <p>{member.role}</p>
+                            <img src={member.image} alt={member.name} tyle={{verticalAlign: 'middle'}} />
                             {/* Below is a bootstrap component that would look cool to put everyone's name, role, and button redirecting to LinkedIn profile */}
                             {/* <Card style={{ width: '18rem' }}>
                                 <Card.Img variant="top" src="/soccerBall.jpeg" />
