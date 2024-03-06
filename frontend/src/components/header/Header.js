cfa368e466be86821ee41b00914ca93b97ca00ad
jackgetgithub <jackgaoobms2@outlook.com>
2024-03-05 19:44:28 -0500

    just header linking to home

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,34 +1,17 @@
 import React from 'react';
 import "./header.css";
 import Navbar from 'react-bootstrap/Navbar';
-import Button from 'react-bootstrap/Button';
-
-const aboutus = (event) => {
-  event.preventDefault();
-  window.location.href = "/AboutPage";
-}
-function handleMouseEnter(event) {
-  event.target.style.backgroundColor = '#3dea76'; // Change background color on hover
-}
-function handleMouseLeave(event) {
-  event.target.style.backgroundColor = '#efeee9'; // Change background color back to normal when mouse leaves
-}
+import Nav from'react-bootstrap/Nav';
 
 const Header = () => {
   return (
-    <div>
-      <Navbar expand="lg" className="bg-body-tertiary">
-        <Navbar.Brand>
-          Soccer Guesser
-          {window.location.pathname == "/" && (<Button onClick={aboutus}
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
-            style={{ position: 'absolute', top: '15px', right: '20px', padding: '10px', backgroundColor: '#efeee9', color: '#000', border: 'none', cursor: 'pointer' }}>
-            About Us
-          </Button>)}
-        </Navbar.Brand>
-      </Navbar>
-    </div>
+  <div>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Navbar.Brand>
+        <Nav.Link href="/">SOCCERDLE</Nav.Link>
+      </Navbar.Brand>
+    </Navbar>
+  </div>
   )
 }
 
