ee55b1ee0d67c06ed08fa803084abe26c4b14a36
hussen2003 <hussenpremier03@gmail.com>
2024-04-02 22:22:32 -0400

    NavBar updates

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -7,39 +7,38 @@ import Form from'react-bootstrap/Form';
 import Button from'react-bootstrap/Button';
 
 const Header = () => {
-  // const aboutus = async (event) => {
-  //   event.preventDefault();
-  //   window.location.href = "/AboutPage";
-  // }
   const name = (JSON.parse(sessionStorage.getItem('user_data')).name);
   const logout = (event) => {
     event.preventDefault();
     window.location.href = "/";
-}
+  }
   return (
-  <div>
-    <Navbar bg="dark" data-bs-theme="dark">
-      <Container>
-        <Navbar.Brand className="navbarBrand" href="/LandingPage">S O C C E R D L E</Navbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link href="/DailyPage">Daily</Nav.Link>
-          <Nav.Link href="/UnlimitedMode">Unlimited</Nav.Link>
-          <Nav.Link href="/Leaderboard">Leaderboard</Nav.Link>
-          <Nav.Link href="/AllTimeLB">All Time Leaderboard</Nav.Link>
-          <Nav.Link href="/AboutPage">About us</Nav.Link>
-        </Nav>
-        <Navbar.Text style={{ padding: '0 20px' }}>
-            Signed in as: <a>{name}</a>
-          </Navbar.Text>
-        <Form className="d-flex">
-          <Button onClick={logout} variant="outline-danger">Sign Out</Button>
-        </Form>
-      </Container>
-    </Navbar>
-    <div className="content">
+    <div>
+      <Navbar bg="dark" data-bs-theme="dark" expand="lg">
+        <Container>
+          <Navbar.Brand className="navbarBrand" href="/LandingPage">S O C C E R D L E</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/DailyPage">Daily</Nav.Link>
+              <Nav.Link href="/UnlimitedMode">Unlimited</Nav.Link>
+              <Nav.Link href="/Leaderboard">Leaderboard</Nav.Link>
+              <Nav.Link href="/AllTimeLB">All Time Leaderboard</Nav.Link>
+              <Nav.Link href="/AboutPage">About us</Nav.Link>
+            </Nav>
+            <Navbar.Text style={{ padding: '0 20px' }}>
+              Signed in as: <a>{name}</a>
+            </Navbar.Text>
+            <Form className="d-flex">
+              <Button onClick={logout} variant="outline-danger">Sign Out</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <div className="content">
         
       </div>
-  </div>
+    </div>
   )
 }
 
