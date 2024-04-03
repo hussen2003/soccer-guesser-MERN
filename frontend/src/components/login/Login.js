05ccd52d92f0d1d9488a53a5874ed48818c61668
hussen2003 <hussenpremier03@gmail.com>
2024-04-03 16:21:55 -0400

    login/signup design updates

--- frontend/src/components/login/Login.js
+++ frontend/src/components/login/Login.js
@@ -1,54 +1,51 @@
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
-import './login.css';
 import Alert from 'react-bootstrap/Alert';
+import './login.css';
 
-const app_name = 'soccerdle-mern-ace81d4f14ec'
-function buildPath(route)
-{
-  if (process.env.NODE_ENV === 'production')
-  {
+const app_name = 'soccerdle-mern-ace81d4f14ec';
+function buildPath(route) {
+  if (process.env.NODE_ENV === 'production') {
     return 'https://' + app_name + '.herokuapp.com/' + route;
-  }
-  else
-  {
+  } else {
     return 'http://localhost:5001/' + route;
   }
 }
-function Login() {
-  const [error, setError] = useState("");
-  const signup = (event) => {
-    event.preventDefault();
-    setMessage("success?");
-    window.location.href = "/SignUpPage";
-  }
-  var loginName;
-  var loginPassword;
-  const [message, setMessage] = useState("");
 
-  const aboutus = async (event) => {
-    event.preventDefault();
-    setMessage("success?");
-    window.location.href = "/AboutPage";
-  }
+function Login() {
+  const [values, setValues] = useState({
+    username: '',
+    password: '',
+  });
+  const [errors, setErrors] = useState({
+    username: '',
+    password: '',
+  });
+  const [message, setMessage] = useState('');
 
   const doLogin = async (event) => {
     event.preventDefault();
-    var obj = { username: loginName.value, password: loginPassword.value };
+
+    var obj = { username: values.username, password: values.password };
     var js = JSON.stringify(obj);
     try {
       const response = await fetch(buildPath('api/auth/login'), {
-        method: "POST",
+        method: 'POST',
         body: js,
-        headers: { "Content-Type": "application/json" },
+        headers: { 'Content-Type': 'application/json' },
       });
       var res = JSON.parse(await response.text());
 
       if (res.error) {
-        setError(res.error);
+        if (res.error === 'Username does not exist') {
+          setErrors({ ...errors, username: 'Username does not exist' });
+        } else if (res.error === 'Incorrect password') {
+          setErrors({ ...errors, password: 'Incorrect password' });
+        } else {
+          setMessage(res.error);
+        }
         return;
-      } 
-      else {
+      } else {
         var user = {
           _id: res._id,
           name: res.name,
@@ -56,66 +53,71 @@ function Login() {
           username: res.username,
           score: res.score,
         };
-        sessionStorage.setItem("user_data", JSON.stringify(user));
-        setMessage("success?");
-        window.location.href = "/LandingPage";
+        sessionStorage.setItem('user_data', JSON.stringify(user));
+        window.location.href = '/LandingPage';
       }
     } catch (e) {
-      alert(e.toString());
+      setMessage('Internal Server Error');
       return;
     }
   };
 
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+    setErrors({ ...errors, [name]: '' });
+  };
+
   return (
-    <div>
-    <div className='login-container'>
+    <div className="login-container">
       <h2>Login</h2>
-      <div style={{ margin: '20px 0' }}></div> { }
-      {(error==="Email not verified") && (<Alert key={'danger'} variant={'danger'}>
-          {error}
-        </Alert>)}
+      {message && (
+        <Alert variant="danger">
+          <Alert.Heading>Error!</Alert.Heading>
+          <p>{message}</p>
+        </Alert>
+      )}
       <form onSubmit={doLogin}>
-        <div className="form-group"> {/* Added a class for consistency */}
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            value={loginName}
-            ref={(c) => (loginName = c)}
-            required
-          />
-          {(error==="Username does not exist") && (<p style={{ color: 'red' , fontWeight: 'bold'}}>{error}</p>)}
+        <div className="form-group">
+          <label>Username</label>
+          <input type="text" name="username" placeholder="Enter your username" value={values.username} onChange={onChange} required />
+          <div className="error" style={{ color: 'red', display: errors.username ? 'block' : 'none' }}>
+            {errors.username}
+          </div>
         </div>
-        <div className="form-group"> {/* Added a class for consistency */}
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={loginPassword}
-            ref={(c) => (loginPassword = c)}
-            required
-          />
-          {(error==="Incorrect password") && (<p style={{ color: 'red' , fontWeight: 'bold'}}>{error}</p>)}
+        <div className="form-group">
+          <label>Password</label>
+          <input type="password" name="password" placeholder="Enter your password" value={values.password} onChange={onChange} required />
+          <div className="error" style={{ color: 'red', display: errors.password ? 'block' : 'none' }}>
+            {errors.password}
+          </div>
         </div>
-        <Button type="submit" variant="outline-primary" onClick={doLogin} style={{
-          padding: '10px 20px',
-          backgroundColor: '#90ee90',
-          color: 'rgb(0, 0, 0)',
-          fontSize: '15px',
-          cursor: 'pointer',
-          width: '150px',
-          height: '40px',
-          transition: 'background-color 0.3s'
-        }}
-          onMouseEnter={(e) => e.target.style.backgroundColor = '#4CAF50'}
-          onMouseLeave={(e) => e.target.style.backgroundColor = '#90ee90'}>
-          Login</Button>{' '}
-        <div style={{ margin: '20px 0' }}></div> { }
-        <p>Don't have an account? <a href="/SignUpPage">Sign Up!</a></p>
+        <Button
+          type="submit"
+          variant="outline-primary"
+          style={{
+            padding: '10px 20px',
+            backgroundColor: '#90ee90',
+            color: 'rgb(0, 0, 0)',
+            fontSize: '15px',
+            cursor: 'pointer',
+            width: '150px',
+            height: '40px',
+            transition: 'background-color 0.3s',
+            width: 'calc(100% - 20px)',
+          }}
+          onMouseEnter={(e) => (e.target.style.backgroundColor = '#4CAF50')}
+          onMouseLeave={(e) => (e.target.style.backgroundColor = '#90ee90')}
+        >
+          Login
+        </Button>
+        <div style={{ margin: '20px 0' }}></div> {}
+        <p>
+          Don't have an account? <a href="/SignUpPage">Sign Up</a>
+        </p>
       </form>
     </div>
-  </div>
   );
-};
+}
 
 export default Login;
--- frontend/src/components/login/Signup.js
+++ frontend/src/components/login/Signup.js
@@ -1,144 +1,190 @@
-import './signup.css';
+import { useState } from 'react';
+import Alert from 'react-bootstrap/Alert';
 import Button from 'react-bootstrap/Button';
-import React, { useState } from 'react'; 
-import Alert from'react-bootstrap/Alert';
-import Spinner from 'react-bootstrap/Spinner';
+import './signup.css';
 
-const app_name = 'soccerdle-mern-ace81d4f14ec'
-function buildPath(route)
-{
-  if (process.env.NODE_ENV === 'production')
-  {
+const app_name = 'soccerdle-mern-ace81d4f14ec';
+function buildPath(route) {
+  if (process.env.NODE_ENV === 'production') {
     return 'https://' + app_name + '.herokuapp.com/' + route;
-  }
-  else
-  {
+  } else {
     return 'http://localhost:5001/' + route;
   }
 }
 
-function SignUp() {
-    const [SignUpName, setSignUpName] = useState("");
-    const [SignUpPassword, setSignUpPassword] = useState("");
-    const [SignUpUsername, setSignUpUsername] = useState("");
-    const [SignUpEmail, setSignUpEmail] = useState("");
-    const [message, setMessage] = useState("");
-    const [error, setError] = useState("");
-    const [show, setShow] = useState(true);
+const SignUp = () => {
+  const [values, setValues] = useState({
+    name: '',
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+  const [errors, setErrors] = useState({
+    name: false,
+    username: false,
+    email: false,
+    password: false,
+    confirmPassword: false,
+  });
 
-    const aboutus = async (event) => {
-      event.preventDefault();
-      setMessage("success?");
-      window.location.href = "/AboutPage";
-    }
+  const [error, setError] = useState('');
 
-    const doSignup = async (event) => {
-      event.preventDefault();
-      var obj = { name: SignUpName, email: SignUpEmail, username: SignUpUsername, password: SignUpPassword }; // maybe SignUpName.target.value
-      var js = JSON.stringify(obj);
-      try {
-        const response = await fetch(buildPath('api/auth/signup'), {
-          method: "POST",
-          body: js,
-          headers: { "Content-Type": "application/json" },
-        });
-        var res = JSON.parse(await response.text());
-  
-        if (res.error) {
-          setError(res.error);
-          return;
-        } else {
-          var user = {
-            _id: res._id,
-            name: res.name,
-            email: res.email,
-            username: res.username,
-            score: res.score,
-          };
-          sessionStorage.setItem("user_data", JSON.stringify(user));
-          setMessage("success?");
-          window.location.href = "/";
-          //alert(res.error);
-          setError(error);
-        }
-      } catch (e) {
-        alert(e.toString());
+  const inputs = [
+    {
+      id: 0,
+      name: 'name',
+      type: 'text',
+      placeholder: 'Enter your full name',
+      errorMessage: 'Name is required!',
+      label: 'Name',
+      required: true,
+    },
+    {
+      id: 1,
+      name: 'username',
+      type: 'text',
+      placeholder: 'Enter your username',
+      errorMessage: 'Username is required',//"Username should be 3-16 characters and shouldn't include any special character!",
+      label: 'Username',
+      //pattern: '^[A-Za-z0-9]{3,16}$',
+      required: true,
+    },
+    {
+      id: 2,
+      name: 'email',
+      type: 'email',
+      placeholder: 'Enter your email',
+      errorMessage: 'It should be a valid email address!',
+      label: 'Email',
+      required: true,
+    },
+    {
+      id: 3,
+      name: 'password',
+      type: 'password',
+      placeholder: 'Enter your password',
+      errorMessage: 'Password should be minimum 6 characters',//'Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!',
+      label: 'Password',
+      pattern: `^.{6,}$`,//`^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,20}$`
+      required: true,
+    },
+    {
+      id: 4,
+      name: 'confirmPassword',
+      type: 'password',
+      placeholder: 'Enter your password again',
+      errorMessage: "Passwords don't match!",
+      label: 'Confirm Password',
+      pattern: values.password,
+      required: true,
+    },
+  ];
+
+  const doSignup = async (event) => {
+    event.preventDefault();
+    var obj = { name: values.name, email: values.email, username: values.username, password: values.password };
+    var js = JSON.stringify(obj);
+    try {
+      const response = await fetch(buildPath('api/auth/signup'), {
+        method: 'POST',
+        body: js,
+        headers: { 'Content-Type': 'application/json' },
+      });
+      var res = JSON.parse(await response.text());
+
+      if (res.error) {
+        setError(res.error);
         return;
+      } else if (res.errors) {
+        setError(res.errors);
+        return;
+      } else {
+        var user = {
+          _id: res._id,
+          name: res.name,
+          email: res.email,
+          username: res.username,
+          score: res.score,
+        };
+        sessionStorage.setItem('user_data', JSON.stringify(user));
+        window.location.href = '/';
       }
-    };
-    const goHome = () => {
-        window.location.href = "/"; 
-    };
+    } catch (e) {
+      alert(e.toString());
+      return;
+    }
+  };
 
-    return (
-        <div className="signup-container"> 
-            <h2>Sign Up</h2> 
-            {((error !== "Email already exists") || (error !== "Username already exists")) && (error === "Internal Server Error") && (<p style={{ color: 'red' , fontWeight: 'bold'}}>Make sure every field is filled out</p>)}
-            {(error==="User created successfully, verification email sent") && (<Alert variant="success">
-              <Alert.Heading>GOAL!</Alert.Heading>
-              <p>
-                User created successfully. Navigate to your email to verify your account.
-              </p>
-            </Alert>)}
-            <form onSubmit={doSignup}> 
-                <div className="form-group">
-                    <label htmlFor="name">Name:</label>
-                    <input
-                        type="text"
-                        id="name" 
-                        value={SignUpName} 
-                        // ref={(c) => (SignUpName = c)}
-                        onChange={(e) => setSignUpName(e.target.value)}
-                        required
-                    />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="username">Username:</label>
-                    <input
-                        type="text"
-                        id="username" 
-                        value={SignUpUsername} 
-                        onChange={(e) => setSignUpUsername(e.target.value)}
-                        required
-                    />
-                    {error && (error==="Username already exists") && (<p style={{ color: 'red' , fontWeight: 'bold'}}>{error}</p>)}
-                </div>
-                <div className="form-group"> 
-                    <label htmlFor="email">Email:</label> 
-                    <input
-                        type="email" 
-                        id="email" 
-                        value={SignUpEmail} 
-                        // ref={(c) => (SignUpEmail = c)}
-                        onChange={(e) => setSignUpEmail(e.target.value)}
-                        required
-                    />
-                    {error && (error==="Email already exists") && (<p style={{ color: 'red' , fontWeight: 'bold'}}>{error}</p>)}
-                </div>
-                <div className="form-group"> 
-                    <label htmlFor="password">Password:</label> 
-                    <input
-                        type="password" 
-                        id="password" 
-                        value={SignUpPassword} 
-                        // ref={(c) => (SignUpPassword = c)}
-                        onChange={(e) => setSignUpPassword(e.target.value)}
-                        required
-                    />
-                </div>
-                <Button onClick = {doSignup}
-                onMouseEnter={(e) => e.target.style.backgroundColor = '#4CAF50'}
-                onMouseLeave={(e) => e.target.style.backgroundColor = '#90ee90'}
-                style={{ position: 'relative', top: '1vh', right: '5px', padding: '10px 20px', backgroundColor: '#90ee90', color: 'rgb(0, 0, 0)', fontSize: '15px', width: '100px', height: '40px', transition: 'background-color 0.3s', border: 'none', margin: '13px 0'}}
-                >Sign Up</Button>
-                <Button onClick = {goHome}
-                onMouseEnter={(e) => e.target.style.backgroundColor = '#db2e2e'}
-                onMouseLeave={(e) => e.target.style.backgroundColor = '#ed6d6d'}
-                style={{ position:'relative', top: '1vh', right: '-10px', padding: '10px 20px', backgroundColor: '#ed6d6d', color: 'rgb(0, 0, 0)', fontSize: '15px', width: '100px', height: '40px', transition: 'background-color 0.3s', border: 'none',margin: '13px 0'}}
-                >Home</Button>
-            </form>
-        </div>
-    );
-}
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    const input = inputs.find((input) => input.name === name);
+    let isValid = true;
 
-export default SignUp; // Export SignUp component for use in other parts of the application
+    if (input.required && value === '') {
+      isValid = false;
+    }
+    if (input.pattern && !new RegExp(input.pattern).test(value)) {
+      isValid = false;
+    }
+    setValues({ ...values, [name]: value });
+    setErrors({ ...errors, [name]: !isValid });
+  };
+
+  return (
+    <div className="signup-container">
+      <form onSubmit={doSignup}>
+        <h1>Sign Up</h1>
+        {error === 'User created successfully, verification email sent' && (
+          <Alert variant="success">
+            <Alert.Heading>GOAL!</Alert.Heading>
+            <p>User created successfully. Please check your email to verify your account.</p>
+          </Alert>
+        )}
+        {error !== 'User created successfully, verification email sent' && error !== '' && (
+          <Alert variant="danger">
+            <Alert.Heading>Error!</Alert.Heading>
+            <p>{error}</p>
+          </Alert>
+        )}
+        {inputs.map((input) => (
+          <div key={input.id} className="form-group">
+            <label>{input.label}</label>
+            <input type={input.type} name={input.name} placeholder={input.placeholder} value={values[input.name]} onChange={onChange} required={input.required} pattern={input.pattern} />
+            <div className="error" style={{ color: 'red', display: errors[input.name] ? 'block' : 'none' }}>
+              {input.errorMessage}
+            </div>
+          </div>
+        ))}
+        <Button
+          variant="primary"
+          type="submit"
+          style={{
+            padding: '10px 20px',
+            backgroundColor: '#90ee90',
+            color: 'rgb(0, 0, 0)',
+            fontSize: '15px',
+            cursor: 'pointer',
+            width: '150px',
+            height: '40px',
+            transition: 'background-color 0.3s',
+            marginRight: '10px',
+            marginTop: '10px',
+            width: 'calc(100% - 20px)',
+          }}
+        >
+          Sign up
+        </Button>
+        <div style={{ margin: '20px 0' }}></div> {}
+        <p>
+          Already have an account? <a href="/">Login</a>
+        </p>
+      </form>
+      {(error !== 'Email already exists' || error !== 'Username already exists') && error === 'Internal Server Error' && (
+        <p style={{ color: 'red', fontWeight: 'bold' }}>Make sure every field is filled out</p>
+      )}
+    </div>
+  );
+};
+
+export default SignUp;
