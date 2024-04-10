38b8434947461c43fc03a9fe968dcd1294a68e8c
hussen2003 <hussenpremier03@gmail.com>
2024-04-09 23:27:37 -0400

    landingPage updates

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -49,68 +49,71 @@ function LandingPage() {
     event.target.style.backgroundColor = '#ff5555';
   }
   return (
-    <div style={{
-      display: 'flex',
-      justifyContent: 'center',
-    }}>
+    <div>
       <Header />
-      <div style={{
-        display: 'flex',
-        justifyContent: 'center',
-      }}>
-        <Row style={{
-          backgroundColor: 'rgba(33, 37, 41, 0.87)',
-          borderRadius: '10px',
-          backdropFilter: 'blur(1px)',
-          minWidth: '90vw',
-          minHeight: '70vh',
-          margin: 'auto',
-          boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
-          color: 'white',
-        }}>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+      >
+        <Row
+          style={{
+            minHeight: '75vh',
+            minWidth: '90vw',
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            padding: '20px',
+            backgroundColor: 'rgba(33, 37, 41, 0.87)',
+            borderRadius: '10px',
+            backdropFilter: 'blur(1px)',
+            color: 'white',
+          }}
+        >
           <Col>
-            <Card className="bg-dark text-white" style={{ width: '350px', height: '500px', margin: 'auto', marginTop: '60px', padding: '10px 0'}}>
+            <Card className="bg-dark text-white" style={{ width: '300px', height: '500px', margin: 'auto'}}>
               <Card.Img variant="top" src={messi2} />
-              <Card.Body style={{display:'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
+              <Card.Body style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
                 <Card.Title>Daily Game</Card.Title>
                 <Card.Text>Every 24 hours a new player will be randomly selected. Can you guess today's player?</Card.Text>
-                <Button variant="success" onClick={playdaily} style={{marginTop: '5px'}}>
+                <Button variant="success" onClick={playdaily} style={{ marginTop: '5px' }}>
                   Play now
                 </Button>
               </Card.Body>
             </Card>
           </Col>
-          <Col >
-            <Card className="bg-dark text-white" style={{ width: '350px', height: '500px', margin: 'auto', marginTop: '60px', padding: '10px 0' }}>
+          <Col>
+            <Card className="bg-dark text-white" style={{ width: '300px', height: '500px', margin: 'auto'}}>
               <Card.Img variant="top" src={SUI} />
-              <Card.Body style={{display:'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
+              <Card.Body style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
                 <Card.Title>Unlimited Game</Card.Title>
                 <Card.Text>Test your knowledge and go as many times as you want! How many can you guess?</Card.Text>
-                <Button variant="success" onClick={goUnlimited} style={{marginTop: '5px'}}>
+                <Button variant="success" onClick={goUnlimited} style={{ marginTop: '5px' }}>
                   Play now
                 </Button>
               </Card.Body>
             </Card>
           </Col>
-          <Col >
-            <Card className="bg-dark text-white" style={{ width: '350px', height: '500px', margin: 'auto', marginTop: '60px', padding: '10px 0' }}>
+          <Col>
+            <Card className="bg-dark text-white" style={{ width: '300px', height: '500px', margin: 'auto'}}>
               <Card.Img variant="top" src={france2} />
-              <Card.Body style={{display:'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
+              <Card.Body style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
                 <Card.Title>Daily Leaderboard</Card.Title>
                 <Card.Text>Leaderboard for the Daily game mode.</Card.Text>
-                <Button variant="success" onClick={goleaderboard} style={{marginTop: '5px'}}>
+                <Button variant="success" onClick={goleaderboard} style={{ marginTop: '5px' }}>
                   Leaderboard
                 </Button>
               </Card.Body>
             </Card>
           </Col>
           <Col>
-            <Card className="bg-dark text-white" style={{ width: '350px', height: '500px', margin: 'auto', marginTop: '60px', padding: '10px 0' }}>
+            <Card className="bg-dark text-white" style={{ width: '300px', height: '500px', margin: 'auto'}}>
               <Card.Img variant="top" src={brazil2} />
-              <Card.Body style={{display:'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
+              <Card.Body style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
                 <Card.Title>All Time Leaderboard</Card.Title>
                 <Card.Text>Overall Leaderboard. Do you see yourself on it?</Card.Text>
-                <Button variant="success" onClick={goAllTime} style={{marginTop: '5px'}}>
+                <Button variant="success" onClick={goAllTime} style={{ marginTop: '5px' }}>
                   All Time
                 </Button>
               </Card.Body>
